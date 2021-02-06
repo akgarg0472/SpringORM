b1b3ec84e890595ff42bd93d08bc491c087bc9ac
Akhilesh Garg <akgarg0472@gmail.com>
2021-02-06 09:52:57 +0530

    Updated ORM DAO & Configuration XML

--- src/main/java/com/spring/orm/Main.java
+++ src/main/java/com/spring/orm/Main.java
@@ -5,11 +5,31 @@ import com.spring.orm.dao.StudentDAO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
         StudentDAO dao = context.getBean("studentDAO", StudentDAO.class);
-        dao.insert(new Student());
+
+        dao.insert(new Student(2, "Akhilesh Garg", "India"));
+
+        List<Student> studentList = dao.getAllStudents();
+        System.out.println("Before deletion");
+        for (Student s : studentList) {
+            System.out.println(s);
+        }
+
+//        Student student = dao.getStudent(1);
+//        dao.delete(student);
+
+//        System.out.println("After deletion");
+//        studentList = dao.getAllStudents();
+//        for (Student s : studentList) {
+//            System.out.println(s);
+//        }
+        dao.update(new Student(1, "Akhilesh K Garg", "Haryana, India"));
+        System.out.println(dao.getAllStudents());
     }
 }
--- src/main/java/com/spring/orm/dao/StudentDAO.java
+++ src/main/java/com/spring/orm/dao/StudentDAO.java
@@ -3,6 +3,7 @@ package com.spring.orm.dao;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public class StudentDAO {
 
@@ -16,17 +17,43 @@ public class StudentDAO {
         this.hibernateTemplate = hibernateTemplate;
     }
 
+    // insertion operation
     @Transactional
     public int insert(Student student) {
         System.out.println("insert");
         return (Integer) this.hibernateTemplate.save(student);
     }
 
+    // select operation
+    @Transactional
+    public Student getStudent(int studentId) {
+        return this.hibernateTemplate.get(Student.class, studentId);
+    }
+
+    @Transactional
+    public List<Student> getAllStudents() {
+        return this.hibernateTemplate.loadAll(Student.class);
+    }
+
+    // delete operation
     @Transactional
     public void delete(Student student) {
         this.hibernateTemplate.delete(student);
     }
 
+    // update operation
+    @Transactional
+    public void update(Student student) {
+        this.hibernateTemplate.update(student);
+    }
+
+    @Transactional
+    public void update(int studentId, String oldStudentName, String oldStudentAddress) {
+        Student student = new Student(studentId, oldStudentName, oldStudentAddress);
+        update(student);
+    }
+
+    // setter method
     public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
         this.hibernateTemplate = hibernateTemplate;
     }
